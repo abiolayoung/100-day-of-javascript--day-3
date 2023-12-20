c908c1941e4b0f8c7ce448a8c9ce96cf67a16619
Abeeb Adeniyi <145553438+abiolayoung@users.noreply.github.com>
2023-12-20 11:44:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict"
+//typeof operator- this operator return the type of operand
+console.log(typeof false)
+console.log(typeof "abiola"/2222)
+console.log(typeof undefined)
+console.log(typeof Math)
+
+let school = "altchool Africa"
+console.log(typeof school)
+
+//Comparison in JavaScript- they are used to check the greater than, less than, equal to, and not equal to of a number including strings
+//equal to "==", not equal to "!==", greater than ">", less than "<".
+//true means 1 or yes
+//false means 0 or no
+console.log(`z`>`a`)
+console.log("abeeb"< "sheriff")  //in this scenario, it uses each letters to carry out the comparison. since abeeb is lesser than sheriff then it is true
+
+console.log(true==1) // true means 1, and 1 is equal to 1, so it is true
+
+//tripple equality check "===" differentiate the data-type and run the operation, if the data-types are not the same, it will trow false
+console.log(true===1)
+console.log('2' === 2)
+console.log('2'== 2)
